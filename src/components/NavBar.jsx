62b8df270268c26be0cfb672438dc1716b5a3d1d
Jaziel Gutierrez <jazielgutierrez50@gmail.com>
2022-08-23 22:40:51 -0300

    Menu de navegacion

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "./images/logo.jpg";
+const Header = () => {
+    return(
+            <div className="container">
+                <ul className="nav d-flex align-items-center">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#"><img src={logo} width="48" alt="Logo" /></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link link-nav" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link link-nav" href="#">Mascotas</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link link-nav">Alimentos</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link link-nav">Servicio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link link-nav">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+    )
+}
+
+export default Header;
